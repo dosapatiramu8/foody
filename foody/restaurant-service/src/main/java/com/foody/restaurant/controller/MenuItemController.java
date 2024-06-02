7c170532b320b1e28661c90f0f2847769cdaed9d
jthileti <jagadeeshwar.reddy006@gmail.com>
2024-06-02 18:45:22 +0530

    MenuItemController

---
+++ foody/restaurant-service/src/main/java/com/foody/restaurant/controller/MenuItemController.java
@@ -0,0 +1,9 @@
+package com.foody.restaurant.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MenuItemController {
+
+}
